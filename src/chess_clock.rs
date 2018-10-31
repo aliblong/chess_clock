4c9f04d527d09d560e2c84802265a7d16e2ff707
Aaron Liblong <liblonga@hotmail.com>
2018-10-31 15:41:00 -0400

    Lifetime error prevents compilation. Next step is try Arc+Mutex, even though the clock will only ever be accessed by a single thread at a time.

--- src/chess_clock.rs
+++ src/chess_clock.rs
@@ -21,16 +21,19 @@ pub struct ChessClock {
     n_players: usize,
     active: usize,
     time_per_turn: Duration,
-    new_duration_tx: UnboundedSender<Duration>,
-    expire_tx: UnboundedSender<()>,
-    next_rx: Option<UnboundedReceiver<()>>,
     time_last_triggered: Option<Instant>,
-    //clock: Option<Delay>,
+}
+
+pub struct ClockedFuture<'a, F> {
+    clock: &'a mut ChessClock,
+    expire: Box<Future<Item=(), Error=()> + Send>,
+    future: F,
 }
 
 // Duration newtypes
 pub struct BaseTime(pub Duration);
 pub struct TimePerTurn(pub Duration);
+
 
 impl ChessClock {
     /// If passed 0 players for whatever reason, it should panic somewhere down the line.
@@ -39,140 +42,63 @@ impl ChessClock {
         n_players: usize,
         base_time: BaseTime,
         time_per_turn: TimePerTurn,
-        new_duration_tx: UnboundedSender<Duration>,
-        expire_tx: UnboundedSender<()>,
-        next_rx: UnboundedReceiver<()>,
     ) -> ChessClock {
         ChessClock {
             remaining: vec![base_time.0 + time_per_turn.0; n_players],
             n_players: n_players,
             active: 0,
             time_per_turn: time_per_turn.0,
-            new_duration_tx: new_duration_tx,
-            expire_tx: expire_tx,
-            next_rx: Some(next_rx),
             time_last_triggered: None,
         }
     }
 
-    fn launch_timer(&mut self, duration: Duration) -> Result<Action, ()> {
+    // Not sure if it's possible to implement this as a method taking &mut self
+    pub fn bind<F>(&mut self, f: F) -> ClockedFuture<F::Future>
+    where F: IntoFuture<Error = ()> {
+        let duration = self.remaining[self.active];
         let now = Instant::now();
         let expiry_time = now + duration;
         self.time_last_triggered = Some(now);
-        let future_expire =
-            Delay::new(expiry_time)
-                .map_err(|_| ())
-                .into_stream();
-        //let future_recv_next = self.recv_next();
-        //let future_recv_next = self.next_rx;
-        let next_or_expire = StreamNext::new(self.next_rx.take().unwrap(), future_expire)
-            .then(|res| {
-                match res {
-                    Ok(Some((Either::A(_), rx, timer))) => {
-                        self.next_rx = Some(rx);
-                        drop(timer);
-                        ok(Action::Next)
-                    },
-                    Ok(Some((Either::B(_), rx, _))) => {
-                        self.next_rx = Some(rx);
-                        ok(Action::Expire)
-                    },
-                    _ => {
-                        err(())
-                    }
-                }
-            });
-        block_on_all(next_or_expire)
-    }
-
-    pub fn start(&mut self) -> impl Future<Item = (), Error = ()> {
-        let start_duration = self.remaining[0];
-        self.process(start_duration);
-        ok(())
-    }
-
-    fn process(&mut self, duration: Duration) {
-        match self.launch_timer(duration) {
-            Ok(Action::Next) => self.next(),
-            Ok(Action::Expire) => self.expire(),
-            Err(()) => self.error(),
-        }
-    }
-
-    fn expire(&mut self) {
-        self.expire_tx.clone().send(());
-    }
-    fn error(&mut self) {}
-
-    pub fn next(&mut self) {
-        {
-            let active_player_remaining = &mut self.remaining[self.active];
-            let new_remaining = *active_player_remaining + self.time_per_turn
-                - self.time_last_triggered.unwrap().elapsed();
-            *active_player_remaining = new_remaining;
-            // This clone is needed because of how UnboundedSender is defined, I think
-            self.new_duration_tx.clone().send(new_remaining);
-        }
-
-        if self.active == self.n_players - 1 {
-            self.active = 0;
-        } else {
-            self.active += 1;
-        }
-        let active_player_remaining = self.remaining[self.active];
-        self.process(active_player_remaining);
-    }
-}
-
-/// https://github.com/rust-lang-nursery/futures-rs/issues/315#issuecomment-325061072
-/// Helper future to wait for the first item on one of two streams, returning
-/// the item and the two streams when done.
-struct StreamNext<S1, S2> {
-    left: Option<S1>,
-    right: Option<S2>,
-}
-
-impl<S1, S2> StreamNext<S1, S2>
-where
-    S1: Stream,
-    S2: Stream<Error = S1::Error>,
-{
-    fn new(s1: S1, s2: S2) -> StreamNext<S1, S2> {
-        StreamNext {
-            left: Some(s1),
-            right: Some(s2),
+        let expire = Box::new(Delay::new(expiry_time)
+                .map_err(|_| ()));
+        ClockedFuture {
+            clock: self,
+            expire: expire,
+            future: f.into_future(),
         }
     }
 }
 
-impl<S1, S2> Future for StreamNext<S1, S2>
-where
-    S1: Stream,
-    S2: Stream<Error = S1::Error>,
-{
-    type Item = Option<(Either<S1::Item, S2::Item>, S1, S2)>;
-    type Error = S1::Error;
+impl<'a, F> Future for ClockedFuture<'a, F>
+where F: Future<Error = ()> {
+    type Item = Either<F::Item, ()>;
+    type Error = ();
 
     fn poll(&mut self) -> Poll<Self::Item, Self::Error> {
         let item = {
-            let left = self.left.as_mut().unwrap();
-            let right = self.right.as_mut().unwrap();
-            match left.poll()? {
-                Async::Ready(None) => return Ok(Async::Ready(None)),
-                Async::Ready(Some(item)) => Either::A(item),
+            match self.future.poll()? {
+                Async::Ready(item) => {
+                    let clock = &mut self.clock;
+                    let active_player_remaining = &mut clock.remaining[clock.active];
+                    let new_remaining = *active_player_remaining + clock.time_per_turn
+                        - clock.time_last_triggered.unwrap().elapsed();
+                    *active_player_remaining = new_remaining;
+
+                    if clock.active == clock.n_players - 1 {
+                        clock.active = 0;
+                    } else {
+                        clock.active += 1;
+                    }
+                    Either::A(item)
+                },
                 Async::NotReady => {
-                    match right.poll()? {
-                        Async::Ready(None) => return Ok(Async::Ready(None)),
-                        Async::Ready(Some(item)) => Either::B(item),
+                    match self.expire.poll()? {
+                        Async::Ready(()) => Either::B(()),
                         Async::NotReady => return Ok(Async::NotReady),
                     }
                 }
             }
         };
-        Ok(Async::Ready(Some((
-            item,
-            self.left.take().unwrap(),
-            self.right.take().unwrap(),
-        ))))
+        Ok(Async::Ready(item))
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -10,7 +10,7 @@ pub use chess_clock::{ChessClock, BaseTime, TimePerTurn};
 mod tests {
     #[test]
     fn it_works() {
-        use tokio::{run, spawn};
+        use tokio;
         use super::{ChessClock, BaseTime, TimePerTurn};
         use futures::prelude::*;
         use futures::sync::mpsc::unbounded;
@@ -18,33 +18,34 @@ mod tests {
         use std::time::{Duration, Instant};
         use tokio::timer::Delay;
         use futures::future::{self, ok, err, Either, FutureResult};
-        use tokio_current_thread::block_on_all;
+        use tokio_current_thread;
 
-        let (duration_tx, duration_rx) = unbounded();
-        let (expire_tx, expire_rx) = unbounded();
-        let (next_tx, next_rx) = unbounded();
         let mut chess_clock = ChessClock::new(
             2,
             BaseTime(Duration::new(5, 0)),
             TimePerTurn(Duration::new(5, 0)),
-            duration_tx,
-            expire_tx,
-            next_rx,
         );
 
-	let clock = chess_clock.start();
-	let when = Instant::now() + Duration::from_millis(1000);
-	let task = Delay::new(when)
-	    .and_then(|_| {
-		println!("{:?}", duration_rx.wait());
-		next_tx.send(());
-		let now = Instant::now();
-		expire_rx.wait();
-		println!("{:?}", now.elapsed().as_secs());
-		ok(())
-	    })
-	    .map_err(|_| ());
-        let supertask = clock.join(task);
-        block_on_all(supertask);
+        let when = Instant::now() + Duration::from_millis(1000);
+        let task = Delay::new(when)
+            .map_err(|_| ());
+        let clocked_task = chess_clock.bind(task)
+            .then(|res| {
+                match res {
+                    Ok(Either::A(_)) => {
+                        println!("Task succeeded");
+                        ok(())
+                    },
+                    Ok(Either::B(_)) => {
+                        println!("Task timed out");
+                        ok(())
+                    },
+                    _ => {
+                        err(())
+                    }
+                }
+            }
+            );
+        tokio::run(clocked_task);
     }
 }
