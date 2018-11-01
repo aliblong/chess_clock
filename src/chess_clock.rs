1cb67fc16c17372e35d422eac4ed45ce34a1641e
Aaron Liblong <liblonga@hotmail.com>
2018-11-01 00:45:18 -0400

    WIP cleanup

--- src/chess_clock.rs
+++ src/chess_clock.rs
@@ -1,23 +1,16 @@
-use futures::future::{self, ok, err, Either, FutureResult};
 use futures::prelude::*;
-use std::iter::Cycle;
-use futures::sync::mpsc::{UnboundedReceiver, UnboundedSender};
-use std::thread;
 use std::time::{Duration, Instant};
-use tokio;
-use tokio_current_thread::block_on_all;
-use tokio::prelude::*;
-use tokio::timer::Delay;
 use std::sync::{Arc, Mutex};
 
-enum Action {
-    Next,
-    Expire,
-}
+use tokio::timer::Delay;
+
+// Duration newtypes
+pub struct BaseTime(pub Duration);
+pub struct TimePerTurn(pub Duration);
 
 /// There's probably a design involving a cycle iterator and
 /// `[rental](https://crates.io/crates/rental)`, but that seems like overengineering at this point
-pub struct ChessClock {
+struct ClockCore {
     remaining: Vec<Duration>,
     n_players: usize,
     active: usize,
@@ -25,26 +18,14 @@ pub struct ChessClock {
     time_last_triggered: Option<Instant>,
 }
 
-pub struct ClockedFuture<F> {
-    clock: Arc<Mutex<ChessClock>>,
-    expire: Box<Future<Item=(), Error=()> + Send>,
-    future: F,
-}
-
-// Duration newtypes
-pub struct BaseTime(pub Duration);
-pub struct TimePerTurn(pub Duration);
-
-
-impl ChessClock {
+impl ClockCore {
     /// If passed 0 players for whatever reason, it should panic somewhere down the line.
-    /// Per FIDE rules, time per turn is added on the very first move
-    pub fn new(
+    fn new(
         n_players: usize,
         base_time: BaseTime,
         time_per_turn: TimePerTurn,
-    ) -> ChessClock {
-        ChessClock {
+    ) -> ClockCore {
+        ClockCore {
             remaining: vec![base_time.0 + time_per_turn.0; n_players],
             n_players: n_players,
             active: 0,
@@ -52,14 +33,26 @@ impl ChessClock {
             time_last_triggered: None,
         }
     }
+}
+
+pub struct ChessClock(Arc<Mutex<ClockCore>>);
+
+impl ChessClock {
+    pub fn new(
+        n_players: usize,
+        base_time: BaseTime,
+        time_per_turn: TimePerTurn,
+    ) -> ChessClock {
+        ChessClock(Arc::new(Mutex::new(ClockCore::new(n_players, base_time, time_per_turn))))
+    }
 
     // Not sure if it's possible to implement this as a method taking &mut self
-    pub fn bind<F>(safe_clock: Arc<Mutex<ChessClock>>, f: F) -> ClockedFuture<F::Future>
+    pub fn bind<F>(self, f: F) -> ClockedFuture<F::Future>
     where F: IntoFuture<Error = ()> {
         let now = Instant::now();
         let mut expiry_time = now;
         {
-            let clock = &mut *safe_clock.lock().unwrap();
+            let clock = &mut *self.0.lock().unwrap();
             let duration = clock.remaining[clock.active];
             clock.time_last_triggered = Some(now);
             expiry_time += duration;
@@ -67,23 +60,29 @@ impl ChessClock {
         let expire = Box::new(Delay::new(expiry_time)
                 .map_err(|_| ()));
         ClockedFuture {
-            clock: safe_clock,
+            clock: self,
             expire: expire,
             future: f.into_future(),
         }
     }
 }
 
+pub struct ClockedFuture<F> {
+    clock: ChessClock,
+    expire: Box<Future<Item=(), Error=()> + Send>,
+    future: F,
+}
+
 impl<F> Future for ClockedFuture<F>
 where F: Future<Error = ()> {
-    type Item = Either<F::Item, ()>;
+    type Item = Option<F::Item>;
     type Error = ();
 
     fn poll(&mut self) -> Poll<Self::Item, Self::Error> {
         let item = {
             match self.future.poll()? {
                 Async::Ready(item) => {
-                    let mut clock = &mut *self.clock.lock().unwrap();
+                    let mut clock = &mut *self.clock.0.lock().unwrap();
                     let active_player_remaining = &mut clock.remaining[clock.active];
                     let new_remaining = *active_player_remaining + clock.time_per_turn
                         - clock.time_last_triggered.unwrap().elapsed();
@@ -94,11 +93,11 @@ where F: Future<Error = ()> {
                     } else {
                         clock.active += 1;
                     }
-                    Either::A(item)
+                    Some(item)
                 },
                 Async::NotReady => {
                     match self.expire.poll()? {
-                        Async::Ready(()) => Either::B(()),
+                        Async::Ready(()) => None,
                         Async::NotReady => return Ok(Async::NotReady),
                     }
                 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,57 @@
+//! A futures-based chess clock which provides timeout-like functionality for other futures.
+//! In addition to the typical configurable base time and time per turn, `ChessClock` supports [1,
+//! usize::max] players, which can be useful in time-controlled, turn-based games that support more
+//! than two players, such as [Hanabi](https://hanabi.live).
+//! Per FIDE rules, time per turn is added on the very first move
+//!
+//! # Example usage:
+//! ```
+//!   extern crate futures;
+//!   extern crate tokio;
+//!   extern crate chess_clock;
+//!
+//!   use chess_clock::{ChessClock, BaseTime, TimePerTurn};
+//!
+//!   use std::thread::sleep;
+//!   use std::time::{Duration, Instant};
+//!
+//!   use futures::{prelude::*, future::{self, ok, err, Either, FutureResult}};
+//!   use tokio;
+//!   use tokio::timer::Delay;
+//!
+//!   let mut clock = ChessClock::new(
+//!       2,
+//!       BaseTime(Duration::new(2, 0)),
+//!       TimePerTurn(Duration::new(2, 0)),
+//!   );
+//!
+//!   let when = Instant::now() + Duration::from_millis(2000);
+//!   let task = Delay::new(when)
+//!       .map_err(|_| ());
+//!   let clocked_task = clock.bind(task)
+//!       .then(|res| {
+//!           match res {
+//!               Ok(Some(_)) => {
+//!                   println!("Task succeeded");
+//!                   ok(())
+//!               },
+//!               Ok(None) => {
+//!                   println!("Task timed out");
+//!                   ok(())
+//!               },
+//!               _ => {
+//!                   err(())
+//!               }
+//!           }
+//!       }
+//!       );
+//!   tokio::run(clocked_task);
+//! ```
+//! Output: `Task succeeded`, since the example task has duration less than the first player's base
+//! time + time per turn
+
 extern crate futures;
-extern crate stream_cancel;
 extern crate tokio;
-extern crate tokio_current_thread;
 
 mod chess_clock;
 pub use chess_clock::{ChessClock, BaseTime, TimePerTurn};
@@ -10,34 +60,32 @@ pub use chess_clock::{ChessClock, BaseTime, TimePerTurn};
 mod tests {
     #[test]
     fn it_works() {
-        use tokio;
         use super::{ChessClock, BaseTime, TimePerTurn};
-        use futures::prelude::*;
-        use futures::sync::mpsc::unbounded;
+
         use std::thread::sleep;
         use std::time::{Duration, Instant};
-        use tokio::timer::Delay;
-        use futures::future::{self, ok, err, Either, FutureResult};
-        use tokio_current_thread;
-        use std::sync::{Arc, Mutex};
 
-        let mut chess_clock = Arc::new(Mutex::new(ChessClock::new(
+        use futures::{prelude::*, future::{self, ok, err, Either, FutureResult}};
+        use tokio;
+        use tokio::timer::Delay;
+
+        let mut clock = ChessClock::new(
             2,
             BaseTime(Duration::new(2, 0)),
             TimePerTurn(Duration::new(2, 0)),
-        )));
+        );
 
         let when = Instant::now() + Duration::from_millis(2000);
         let task = Delay::new(when)
             .map_err(|_| ());
-        let clocked_task = ChessClock::bind(chess_clock, task)
+        let clocked_task = clock.bind(task)
             .then(|res| {
                 match res {
-                    Ok(Either::A(_)) => {
+                    Ok(Some(_)) => {
                         println!("Task succeeded");
                         ok(())
                     },
-                    Ok(Either::B(_)) => {
+                    Ok(None) => {
                         println!("Task timed out");
                         ok(())
                     },
